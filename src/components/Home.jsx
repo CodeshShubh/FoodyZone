b6ba73ca00146b51aef4087cdbca667ffd6fb2e9
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-06-19 12:54:04 +0530

    making of foods card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,152 @@
-import React from 'react'
+import styled from "styled-components";
+import bg from '../assets/Background.png'
+import img1 from '../assets/dish1.png'
+import img2 from '../assets/dish6.png'
+import img3 from '../assets/dish5.png'
+import img4 from '../assets/dish4.png'
+import img5 from '../assets/dish3.png'
+import img6 from '../assets/dish2.png'
+
+
+import {Button} from './NavBar'
+
+
+const foods = [
+  {
+    image: img1,
+    heading: "Boiled Eggs",
+    text: 'Lorem ipsum dolor sit amet consectetur. Odio elementum in neque cras eget est.',
+    btntext: 10.00
+     
+  },
+  {
+    image: img1,
+    heading: "Boiled Eggs",
+    text: 'Lorem ipsum dolor sit amet consectetur. Odio elementum in neque cras eget est.',
+    btntext: 10.00
+     
+  },
+  {
+    image: img1,
+    heading: "Boiled Eggs",
+    text: 'Lorem ipsum dolor sit amet consectetur. Odio elementum in neque cras eget est.',
+    btntext: 10.00
+     
+  },
+  {
+    image: img1,
+    heading: "Boiled Eggs",
+    text: 'Lorem ipsum dolor sit amet consectetur. Odio elementum in neque cras eget est.',
+    btntext: 10.00
+     
+  },
+  {
+    image: img1,
+    heading: "Boiled Eggs",
+    text: 'Lorem ipsum dolor sit amet consectetur. Odio elementum in neque cras eget est.',
+    btntext: 10.00
+     
+  },
+  {
+    image: img1,
+    heading: "Boiled Eggs",
+    text: 'Lorem ipsum dolor sit amet consectetur. Odio elementum in neque cras eget est.',
+    btntext: 10.00
+     
+  },
+] 
+
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <HomeConatiner>
+      <FoodCardConatiner>
+      {
+        foods.map(({image, heading, text, btntext})=>
+          <div key={heading} className="food_card">
+        <div className="food_image">
+        <img src={image} alt="Food Image" />
+        </div>
+        <div className="food_info">
+          <h1>{heading}</h1>
+          <p>{text}</p>
+          <div className="food_Card_btn"><FoodCardBtn>${btntext}.00</FoodCardBtn></div>
+        </div>
+      </div>)
+      }    
+
+      </FoodCardConatiner>
+    </HomeConatiner>
   )
 }
 
-export default Home
+export default Home;
+
+
+const HomeConatiner = styled.div`
+  max-width: 1440px;
+  min-height: calc(785px - 220px);
+  background-size: cover;
+  background: linear-gradient(rgba(0, 0, 0, 0.5),  rgba(0, 0, 0, 0.5)) , url(${bg});
+
+  
+`
+
+const FoodCardConatiner =  styled.div`
+  border: 1px solid blue;
+  padding: 1rem;
+  max-width: 1060px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  row-gap: 32px;
+  column-gap: 20px;
+  justify-content: center;
+  align-items: center;
+.food_card{
+border-radius: 19px;
+background-color: rgba(255, 255, 255, 0.1);
+box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+backdrop-filter: blur(10px);
+-webkit-backdrop-filter: blur(10px);
+max-width: 400px;
+display: flex;
+justify-content: space-between;
+padding: 1rem;
+.food_image{
+  width: 8rem;
+  width: 8rem;
+  >img{
+  width: 9rem;
+  width: 9rem;
+  }
+  
+}
+.food_info{
+    max-width: 220px;
+    min-height: 86px;
+    padding: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    >h1{
+      font-weight: 600;
+      font-size: 1rem;
+      color: white;
+    }
+    >P{
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 1rem;
+      color: white;
+    }
+
+}
+}
+
+`
+const FoodCardBtn = styled(Button)`
+padding: 0.3rem 0.5rem;
+  
+`
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,18 +2,38 @@ import logo from '../assets/Foody Zone.svg';
 import styled from 'styled-components';
 
 const NavBar = () => {
+
+  const btns = [
+    {
+      name: "All",
+      type: "all"
+    },
+    {
+      name: "Breakfast",
+      type: "breakfast"
+    },
+    {
+      name: "Lunch",
+      type: "lunch"
+    },
+    {
+      name: "Dinner",
+      type: "dinner"
+    }
+  ]
   return (
     <NavContainer>
       <div className='nav_top'>
         <img src={logo} alt=""logo />
         <input type='text' name='search' placeholder='Search Food...'/>
       </div>
-      <div>
-      <Button>All</Button>
-      <Button>Breakfast</Button>
-      <Button>Lunch</Button>
-      <Button>Dinner</Button>
-      </div>
+      <ButtonConatiner>
+         {
+          btns.map((value)=>{
+            return <Button key={value.name}>{value.name}</Button>
+          })
+         }
+      </ButtonConatiner>
     </NavContainer>
   )
 }
@@ -29,8 +49,7 @@ const NavContainer = styled.nav`
 
   .nav_top {
     max-width: 1200px;
-    min-height: 40px;
-    margin: 4rem auto;
+    margin: 0 auto 2rem;
     display: flex;
     justify-content: space-between;
     padding: 1rem;
@@ -40,7 +59,8 @@ const NavContainer = styled.nav`
     min-height: 1rem;
     border: 1px solid #FF0909;
     border-radius: 5px;
-    padding: 5px;
+    padding: 6px;
+    color: white;
     &::placeholder{
       color: white;
     }
@@ -49,6 +69,26 @@ const NavContainer = styled.nav`
 
 `;
 
-const Button = styled.button`
+ const ButtonConatiner = styled.div`
+max-width: 326px;
+min-height: 2rem;
+margin: 10px auto;
+display: flex;
+justify-content: center;
+gap: 10px;
+`
+
+export const Button = styled.button`
+background-color: #ff4343;
+padding: 0 0.5rem;
+border: none;
+border-radius: 5px;
+font-weight: 400;
+font-size: 16px;
+color: white;
+&:hover{
+  cursor: pointer;
+  background-color: #ff6f6f;
+}
   
 `
